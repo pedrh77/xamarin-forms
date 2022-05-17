71383f3a1ed6ea0139e230d5e044ce256bd28126
Pedro Santos <pedro.henrique@usabit.com.br>
2022-05-17 20:44:26 -0300

    Adicionando App Falador

---
+++ Notificacoes_2.0/Notificacoes_2.0/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notificacoes_2._0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        double dvolume = 1.0, dtom = 0.75;
+        private async void OnBtn_Falar(object sender, EventArgs e)
+        {
+            var settings = new SpeechOptions()
+            {
+                Volume = Convert.ToSingle(dvolume),
+                Pitch = Convert.ToSingle(dtom)
+            };
+
+            if (Falar.Text == null || string.IsNullOrWhiteSpace(Falar.Text) || string.IsNullOrEmpty(Falar.Text))
+                await TextToSpeech.SpeakAsync("Olá eu sou o App Falador", settings);
+            else await TextToSpeech.SpeakAsync(Falar.Text, settings);
+        }
+
+        private void onVolumeChange(object sender, ValueChangedEventArgs e)
+        {
+            dvolume = e.NewValue;
+            lblvolume.Text = $"Volume da Voz: {e.NewValue}";
+        }
+
+        private void onTomChange(object sender, ValueChangedEventArgs e)
+        {
+            dtom = Convert.ToDouble(e.NewValue);
+            lblTom.Text = $"Tom de : {e.NewValue}";
+        }
+
+
+    }
+}
